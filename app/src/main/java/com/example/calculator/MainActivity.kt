1f0a6299a8895ad8dc2c34b3982d23ce377ec666
nolanturi <nturi@bu.edu>
2023-09-19 02:32:07 -0400

    fixed the failed ref that kept it crashing

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,26 +2,18 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.view.View
+import com.example.calculator.R
 
 class MainActivity : AppCompatActivity() {
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
-
     }
 
-    private fun oneString() {
+    fun equalsInput(view: View) {}
 
-    }
+    fun numberInput(view: View) {}
+
+    fun operandInput(view: View) {}
 }
